4577d12ec2bc7f456022928e2d7f1ac0e169e542
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-30 15:17:30 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// ***middleware pezzo di codice che sta tra 2 processi
+// ***lutente puo vedere una risposta solo se glielo permette auth(controlla se lutente è loggato)
+// ***per vedere dashbord bisogna passare attraverso questo middleware se no ,non si vede niente
+// ***bisogna raggrupare rotte perche hanno cose in comune
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
